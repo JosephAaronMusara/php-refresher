a5d6445cb21ac2611da5401b6aa9982c0a93a0ec
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-10 14:40:52 +0200

    Better syntax to embede php into a page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    $name = "Joseph";
+  if($name == 'Joseph'){?>
+  <p>The name is <?php echo $name?> </p>
+  <?php } else {?>
+    <p>The name is not Joseph</p>
+  <?php }?>
+</body>
+</html>
