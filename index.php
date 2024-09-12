4c28d3e7f4ec55bf001d93612a911991b7875b00
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-12 11:38:07 +0200

    Meaningful UI

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
-
 <body>
-  <h3>Sign Up</h3>
-  <form action="includes/formhandler.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <input type="email" name="email" placeholder="E-mail">
-    <button>Sign Up</button>
-  </form>
-
+  <div class="signup-container">
+    <h3>Sign Up</h3>
+    <form action="includes/formhandler.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <input type="email" name="email" placeholder="E-mail">
+      <button>Sign Up</button>
+    </form>
+  </div>
 </body>
-
 </html>
