6b242cd2148b46c7b5c2e55cc067bef18a9300dd
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-12 14:36:48 +0200

    CUD without R

--- index.php
+++ index.php
@@ -9,11 +9,28 @@
 <body>
   <div class="signup-container">
     <h3>Sign Up</h3>
-    <form action="includes/formhandler.inc.php" method="post">
+    <form action="includes/createuser.inc.php" method="post">
       <input type="text" name="username" placeholder="Username">
       <input type="password" name="pwd" placeholder="Password">
       <input type="email" name="email" placeholder="E-mail">
       <button>Sign Up</button>
+    </form>
+  </div>
+  <div class="signup-container">
+    <h3>Change User</h3>
+    <form action="includes/userupdate.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <input type="email" name="email" placeholder="E-mail">
+      <button>Update</button>
+    </form>
+  </div>
+  <div class="signup-container">
+    <h3>Delete User</h3>
+    <form action="includes/userdelete.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <button>Delete</button>
     </form>
   </div>
 </body>
